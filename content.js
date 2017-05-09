c82f608ebeeae586a94a72965fa4aa71e5327ec9
Aleksandr Ten <aleksandr.ten@fogstream.ru>
2017-05-10 00:48:08 +1000

    first commit

---
+++ content.js
@@ -0,0 +1,20 @@
+loadGifUrl = chrome.runtime.getURL("load.gif");
+// вставка анимации загрузки
+$("html").find(".cCard__Contacts-Revenue-Desktop").find(".cCard__BlockMaskSum").html($('<img>',{width:14, height:14, src: loadGifUrl}))
+$("html").find(".cCard__Owners-Profit-Desktop").find(".cCard__BlockMaskSum").html($('<img>',{width:14, height:14,src: loadGifUrl}))
+$("html").find(".cCard__Reliability-Cost-Desktop").find(".cCard__BlockMaskSum").html($('<img>',{width:14, height:14,src: loadGifUrl}))
+//
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    // замена значений на странице
+    $("html").find(".cCard__Contacts-Revenue-Desktop").find(".cCard__BlockMaskSum").html(request.revenue);
+    $("html").find(".cCard__Owners-Profit-Desktop").find(".cCard__BlockMaskSum").text(request.profit);
+    $("html").find(".cCard__Reliability-Cost-Desktop").find(".cCard__BlockMaskSum").text(request.cost);
+    //
+    $("html").find(".cCard__EconomyResult-Mobile-Revenue").find(".cCard__BlockMaskSum").text(request.revenue);
+    $("html").find(".cCard__EconomyResult-Mobile-Profit").find(".cCard__BlockMaskSum").text(request.profit);
+    $("html").find(".cCard__EconomyResult-Mobile-Cost").find(".cCard__BlockMaskSum").text(request.cost);
+    // ответ
+    if (request.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
+  });
